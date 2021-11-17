ba416f1a0dc22ae3e7b4ff4970352a9166acd45e
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-11-18 00:13:43 +0530

    Mobile number validation

--- RegexLambdaExpression.java
+++ RegexLambdaExpression.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 @FunctionalInterface
-interface EmailId {
-    void validUser(String email);
+interface MobileNumber {
+    void validUser(String mobileNumber);
 }
 
 public class RegexLambdaExpression {
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration program");
-        EmailId function = (email)->{
-            boolean check = Pattern.matches("^([a-z]{3}[.][a-z]{3}@[a-z]{2}[.][a-z]{2}[.][a-z]{2})$",email);
+        MobileNumber function = (mobileNumber)->{
+            boolean check = Pattern.matches("^[0-9]{2}\\s[0-9]{10}$",mobileNumber);
             if (check == true) {
-                System.out.println("Email is Valid");
+                System.out.println("Mobile number is Valid");
             }else
-                System.out.println("Enter valid email");
+                System.out.println("Enter valid mobile number");
         };
-        function.validUser("abc.xyz@bl.co.in");
+        function.validUser("91 8806564783");
     }
 }
