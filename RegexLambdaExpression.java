0ff20ce93bfea75eeb4985817338e372d1d7740f
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-11-17 22:55:11 +0530

    Enter valid First Name of user

---
+++ RegexLambdaExpression.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.lambdaexpression;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@FunctionalInterface
+interface Regex{
+
+    boolean details(String input);
+}
+
+public class RegexLambdaExpression {
+
+    public static void main(String[] args){
+        System.out.println(" Lambda Expression ");
+        Regex regex =(input)->{
+            String pattern= "^[A-z]{3,}";
+            Pattern r= Pattern.compile(pattern);
+
+            Matcher m = r.matcher(input);
+
+
+            if(m.matches()){
+                System.out.println("First Name Matched " );
+
+            }else
+            {
+                System.out.println(" First Name No Matched ");
+            }
+            return true;
+        };
+
+        System.out.println("First Name :"+" "+regex.details("Amol"));
+    }
+}
