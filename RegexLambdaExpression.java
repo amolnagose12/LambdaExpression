895d12937088d457c8d42dce4032d14847b9822b
amolnagose12@gmail.com <amolnagose12@gmail.com>
2021-11-17 23:06:12 +0530

    First Name validation

--- RegexLambdaExpression.java
+++ RegexLambdaExpression.java
@@ -1,35 +1,24 @@
 package com.bridgelabz.lambdaexpression;
 
-import java.util.regex.Matcher;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 @FunctionalInterface
-interface Regex{
-
-    boolean details(String input);
+interface firstname {
+    void validUser(String name);
 }
 
 public class RegexLambdaExpression {
 
-    public static void main(String[] args){
-        System.out.println(" Lambda Expression ");
-        Regex regex =(input)->{
-            String pattern= "^[A-Z]{1}[a-z]{2,}";
-            Pattern r= Pattern.compile(pattern);
-
-            Matcher m = r.matcher(input);
-
-
-            if(m.matches()){
-                System.out.println("First Name Matched " );
-
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration program");
+        firstname function = (name)->{
+            boolean check = Pattern.matches("([A-Z][a-z]{3,})",name);
+            if (check == true) {
+                System.out.println("Firstname is Valid");
             }else
-            {
-                System.out.println(" First Name Not Matched ");
-            }
-            return true;
+                System.out.println("Enter valid name");
         };
-
-        System.out.println("First Name :"+" "+regex.details("amol"));
+        function.validUser("Amol");
     }
 }
